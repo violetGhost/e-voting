8ffbacf1639bc40764b85eaa4e0c7256ffd0a4c7
violetGhost <syazana.suzane@gmail.com>
2021-07-05 03:05:06 +0800

    add finall

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const parseurl = require('parseurl');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -12,26 +13,49 @@ app.use(express.static('build'));
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.get('/', function(req, res) {
     res.json('Works!');
 });
 
+app.get('/api/main', function(req, res) {
+    var usernames = []
+    var final = []
+
+    dbConn.query('SELECT * FROM user ORDER BY id',function(err,rows)     {
+
+        for (i = 0; i < rows.length; i++){
+            usernames[i] = rows[i].username ;
+            final.push({
+                'username': rows[i].username
+            })
+        }
+        res.send(final);
+
+    });
+
+});
+
 app.get('/api/electionName', function(req, res) {
     var electionNames = []
-    var electionOrganizers = []
+    var electionPositions = []
     var electionIds = []
     var final = []
 
-    dbConn.query('SELECT * FROM electionlists ORDER BY id desc',function(err,rows)     {
+    dbConn.query('SELECT * FROM election ORDER BY id desc',function(err,rows)     {
 
         for (i = 0; i < rows.length; i++){
             electionNames[i] = rows[i].election_name ;
-            electionOrganizers[i] = rows[i].election_organizer;
+            electionPositions[i] = rows[i].election_position;
             electionIds[i] = rows[i].election_id;
             final.push({
                 'election_id': rows[i].election_id,
-                'election_organizer': rows[i].election_organizer,
+                'election_position': rows[i].election_position,
                 'election_name': rows[i].election_name
             })
         }
@@ -45,11 +69,11 @@ app.post('/api/electionName', async function(req, res) {
 
     let election_id = Math.floor(Math.random() * 100);
     let election_name = req.body.election_name;
-    let election_organizer = req.body.election_organizer;
-    let election_password = md5(req.body.election_password);
+    let election_position = req.body.election_position;
+    //let election_password = md5(req.body.election_password);
     let error = false;
 
-    var sql = "INSERT INTO electionlists VALUES(0,'"+election_id+"','"+election_name+"','"+election_organizer+"','"+election_password+"')";
+    var sql = "INSERT INTO election VALUES(0,'"+election_id+"','"+election_name+"','"+election_position+"','"+"password"+"','2021-05-13 19:57:24','2021-05-13 19:57:24')";
         dbConn.query(sql, function (err, result) {
             if (err) throw err;
                 console.log("1 election inserted");
@@ -59,17 +83,29 @@ app.post('/api/electionName', async function(req, res) {
 
 });
 
-app.post('/api/adminLogin', async function(req, res) {
-    admin.findOne({
-        username: req.body.username,
-        password: md5(req.body.password),
-    }).then(election => {
-        if(election === null){
-            res.send(false);
-        }else{
-            res.send(true);
-        }
-    });
+app.post('/api/adminLogin', function(request, response) {
+
+    var username = request.body.username;
+    var password = request.body.password;
+    
+    if (username && password) {
+        
+        dbConn.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password],function(error, results, fields)     {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                console.log("success login yeheee " + results.length);
+                response.send(username);
+            } else {
+                console.log("nahhhh ");
+            }
+        });
+
+
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
 });
 
 const port = process.env.PORT || 8000;
