3bad61f1f9d8cba2831e9d93dfe2ae7b5008d53b
violetGhost <syazana.suzane@gmail.com>
2021-07-04 17:10:43 +0800

    change db connection to production

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	password:'18FFaridah',
-	database:'election'
+	host:'us-cdbr-east-04.cleardb.com',
+	user:'b919182a4968ff',
+	password:'decebae2',
+	database:'heroku_831396407'
 });
 connection.connect(function(error){
 	if(!!error) {
